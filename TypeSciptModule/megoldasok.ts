b8c3d70e6f15774d25e6f34b26b9597bdb8922d7
VargaOrsi <orsi0120@gmail.com>
2022-07-28 20:58:59 +0200

    megoldas

--- TypeSciptModule/megoldasok.ts
+++
@@ -1,9 +0,0 @@
-export { };
-//1. feladat
-function Rng(alsoHatar: number, felsoHatar: number): number {
-    return Math.round(Math.random() * (felsoHatar - alsoHatar)) + alsoHatar;
-}
-//2.feladat
-//3.feladat
-//4.fealdat
-//5.feladat
