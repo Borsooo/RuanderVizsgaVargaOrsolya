7f5b7f9c2b936029bce77d3b9ed2da737d05276e
VargaOrsi <orsi0120@gmail.com>
2022-07-28 20:45:58 +0200

    megoldasok

---
+++ TypeSciptModule/megoldasok.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+//1. feladat
+function Rng(alsoHatar, felsoHatar) {
+    return Math.round(Math.random() * (felsoHatar - alsoHatar)) + alsoHatar;
+}
+//2.feladat
+//function TombGenerator(meret, alsoHatar, felsoHatar): number[] {}
+//3.feladat
+//function Duplazo(VizsgaltTomb: number): number[] {}
+//4.fealdat
+//5.feladat
